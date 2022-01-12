04fbcca751a2e57144f2a4a00b0d8f80012f93eb
Heitor Santos <heitorl09@gmail.com>
2022-01-12 15:50:12 -0300

    Ajuste keycloak init

--- keycloak/keycloak.config.sh
+++ keycloak/keycloak.config.sh
@@ -7,11 +7,10 @@ KEYCLOAK_CREATE_REALM=${KEYCLOAK_CREATE_REALM:-"admin"}
 KEYCLOAK_MASTER_USER=${KEYCLOAK_MASTER_USER:-"admin"}
 KEYCLOAK_MASTER_PASSWORD=${KEYCLOAK_MASTER_PASSWORD:-"admin"}
 
-# KEYCLOAK_PROXY_CLIENT_ID=${KEYCLOAK_PROXY_CLIENT_ID:-"client-proxy"}
-# KEYCLOAK_PROXY_CLIENT_SECRET=$(cat .KEYCLOAK_PROXY)
-# KEYCLOAK_PROXY_USER=${KEYCLOAK_PROXY_USER:-"user-proxy"}
-# KEYCLOAK_PROXY_USER_SECRET=$(cat .KEYCLOAK_PROXY)
-
+KEYCLOAK_PROXY_CLIENT_ID=${KEYCLOAK_PROXY_CLIENT_ID:-"client-proxy"}
+KEYCLOAK_PROXY_CLIENT_SECRET=$(cat /secrets/KEYCLOAK_PROXY)
+KEYCLOAK_PROXY_USER=${KEYCLOAK_PROXY_USER:-"user-proxy"}
+KEYCLOAK_PROXY_USER_SECRET=$(cat /secrets/KEYCLOAK_PROXY_USER)
 
 # check if Keycloak is started
 if curl --output /dev/null --silent --head --fail "$KEYCLOAK_HOST"; then
@@ -27,7 +26,7 @@ JWT=''
 
 getToken() {
   echo 'Obtaining access token...'
-
+  
   JWT=$(curl -sS -X POST "${KEYCLOAK_HOST}/realms/master/protocol/openid-connect/token" \
         --data-urlencode "username=${KEYCLOAK_MASTER_USER}" \
         --data-urlencode "password=${KEYCLOAK_MASTER_PASSWORD}" \
@@ -49,28 +48,33 @@ createRealm() {
   curl -X POST "${KEYCLOAK_HOST}/admin/realms" \
     -H "Content-Type:application/json" \
     -H "Authorization: Bearer ${JWT}" \
-    -d "{\"realm\": \"${KEYCLOAK_CREATE_REALM}\", \"enabled\": true}"
+    -d "$(cat /keycloak/customRealmRepresentation.json  | sed 's/$KEYCLOAK_CREATE_REALM/teste/g')"
 
   echo "...finished realm ${KEYCLOAK_CREATE_REALM} create."
 }
 
-createClient() {
-  echo "Creatting Client in realm master..."
-    curl -X POST "${KEYCLOAK_HOST}/admin/realms/master/clients" \
+ createClient() {
+   echo "Creatting Client in realm master..."
+
+  curl -X POST "${KEYCLOAK_HOST}/admin/realms/master/clients" \
     -H "Content-Type:application/json" \
     -H "Authorization: Bearer ${JWT}" \
     -d "{\"enabled\":true,\"attributes\":{},\"redirectUris\":[],\"clientId\":\"${KEYCLOAK_PROXY_CLIENT_ID}\",\"rootUrl\":\"http://localhost:8000/\",\"protocol\":\"openid-connect\"}"
 
-  echo "...finished realm Master create."
-}
+   echo "...finished realm Master create."
+ }
 
 createUser() {
   echo "Creatting User in realm master..."
-    curl -X POST "${KEYCLOAK_HOST}/admin/realms/master/users" \
+    
+  curl -X POST "${KEYCLOAK_HOST}/admin/realms/master/users" \
     -H "Content-Type:application/json" \
     -H "Authorization: Bearer ${JWT}" \
     -d "{\"username\": \"${KEYCLOAK_PROXY_USER}\",
                 \"enabled\": true,
+                \"requiredActions\": [
+                  \"UPDATE_PASSWORD\"
+                ],
                 \"credentials\": [{
                     \"id\": \"0b06ea86-efbc-4c5e-bac2-f5650397defb\",
                     \"type\": \"password\",
@@ -82,8 +86,11 @@ createUser() {
                     \"admin\"
                 ]
             }"
+
   echo "...finished realm Master create."
 }
 
 getToken
 createRealm
+createClient
+createUser
